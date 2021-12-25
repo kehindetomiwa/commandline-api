3b78a7b2847938035c7a0fc6f166575d0c4cad11
Quinix Solution <akgbkebu10000@outlook.com>
2021-12-25 16:58:10 +0100

    model

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using commandline_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using commandline_api.Data;
+
+
+
+namespace commandline_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        //GET api/command
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommand()
+        {
+            var commandItems = _repository.GetAppCommander();
+            return Ok(commandItems);
+
+        }
+
+        //GET api/command/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandByID(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+
+        }
+    }
+}
