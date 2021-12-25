beaf097f855a70b9d717452fca2880b8f0344420
Quinix Solution <akgbkebu10000@outlook.com>
2021-12-25 01:53:37 +0100

    added model

---
+++ Properties/Models/Command.cs
@@ -0,0 +1,10 @@
+namespace commandline_api.Models
+{
+    public class commandline_api
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+    }
+}
